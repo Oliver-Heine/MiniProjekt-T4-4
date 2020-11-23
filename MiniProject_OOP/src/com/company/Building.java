9fc31c13bdfc5d8e82aff16def55f116ca41b294
Jasan <jasanfarah@gmail.com>
2020-11-23 09:03:37 +0100

    Sensor and Building Updates

--- MiniProject_OOP/src/com/company/Building.java
+++ MiniProject_OOP/src/com/company/Building.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class Building {
+
+    private String name;
+    private static int buildingID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static int getBuildingID() {
+        return buildingID;
+    }
+
+    public void setBuildingID(int buildingID) {
+        this.buildingID = buildingID;
+    }
 }
--- MiniProject_OOP/src/com/company/Sensor.java
+++ MiniProject_OOP/src/com/company/Sensor.java
@@ -1,4 +1,33 @@
 package com.company;
 
 public class Sensor {
+    //
+     private String type;
+    private int id;
+    private int buildingID;
+    private double currentSensorValue;
+
+
+    public Sensor(String type, int id, double currentSensorValue) {
+        this.type = type;
+        this.id = id;
+        this.buildingID = Building.getBuildingID();
+        this.currentSensorValue = currentSensorValue;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBuildingID() {
+        return buildingID;
+    }
+
+    public double getCurrentSensorValue() {
+        return currentSensorValue;
+    }
 }
